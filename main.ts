0d0efc3037f787ca70e7c998747f2bd5aaf4e082
ventus.laminae <ventus.laminae@gmail.com>
2023-10-03 16:39:03 +0900

    デプロイテスト
    テスト２

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { serve } from "https://deno.land/std@0.202.0/http/server.ts";
 
 const handler = async (_request: Request): Promise<Response> => {
 
-    return new Response("Hallo Deno 5", {
+    return new Response("Hallo Deno 7", {
         status: 200,
         headers: {
             "content-type": "application/json",
